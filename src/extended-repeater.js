3636961aa59c6303d836799e58d4818f85c381de
ye-russell <bekberata@gmail.com>
2021-03-21 02:14:27 +0600

    feat: complete vigenere and extended-repeat

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (!options.repeatTimes) options.repeatTimes = 1;
+  if (typeof options.addition != "string") options.addition = String(options.addition);
+  if (options.addition == "undefined") options.addition = "";
+  if (!options.additionSeparator) options.additionSeparator = "|";
+  if (!options.additionRepeatTimes) options.additionRepeatTimes = 1;
+  if (!options.separator) options.separator = "+";
+  addition = String(options.addition);
+  afterStr = (addition + String(options.additionSeparator)).repeat(options.additionRepeatTimes);
+  newStr = String(str) + afterStr.substring(0, afterStr.length - options.additionSeparator.length) + options.separator
+  newAddStr = newStr.repeat(options.repeatTimes);
+
+  return newAddStr.substring(0, newAddStr.length - options.separator.length);
 };
   
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,51 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor (isWhat) {
+    this.isWhat = isWhat;
+  }
+  encrypt(str, key) {
+    var thisString = str.toUpperCase();
+    thisString = thisString.split('');
+    var thisKey = key.toUpperCase();
+    thisKey = thisKey.repeat(thisString.length / thisKey.length + 1);
+    thisKey = thisKey.split('');
+    var returnator = '';
+    counter = 0;
+
+    for (i = 0; i < thisString.length; i++) {
+      if (thisString[i].charCodeAt(0) >= 65 &&  thisString[i].charCodeAt(0) <= 90) {
+        returnator = returnator + String.fromCharCode(((thisString[i].charCodeAt(0) + thisKey[counter].charCodeAt(0)) % 26) + 65);
+        counter++;
+      } else {
+        returnator = returnator + thisString[i];
+      }
+    }
+    if (this.isWhat == false) {
+      return thisString.reverse().join('');
+    }
+    return returnator;
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(str, key) {
+    var thisString = str.toUpperCase();
+    thisString = thisString.split('');
+    var thisKey = key.toUpperCase();
+    thisKey = thisKey.repeat(thisString.length / thisKey.length + 1);
+    thisKey = thisKey.split('');
+    var returnator = '';
+    counter = 0;
+    for (i = 0; i < thisString.length; i++) {
+      if (thisString[i].charCodeAt(0) >= 65 &&  thisString[i].charCodeAt(0) <= 90) {
+        returnator = returnator + String.fromCharCode(((thisString[i].charCodeAt(0) + 26 - thisKey[counter].charCodeAt(0)) % 26) + 65);
+        counter++;
+      } else {
+        returnator = returnator + thisString[i];
+      }
+    }
+    if (this.isWhat == false) {
+      return thisString.reverse().join('');
+    }
+    return returnator;
   }
 }
 
